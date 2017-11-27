0e26a6e1ec13a50fab69fff77cd73e08c7ff3678
Antoine-Auffret <antoine@auffret.ovh>
2017-11-27 09:38:18 +0100

    Get the informations of a job by exec_id (#7)
    * Get the informations of a job by exec_id
    
    * resolve conflicts

--- pyrundeck/rundeck.py
+++ pyrundeck/rundeck.py
@@ -144,6 +144,10 @@ class Rundeck():
     def execution_output_by_id(self, exec_id):
         url = '{}/execution/{}/output'.format(self.API_URL, exec_id)
         return self.__get(url)
+        
+    def execution_info_by_id(self, exec_id):
+        url = '{}/execution/{}'.format(self.API_URL, exec_id)
+        return self.__get(url)
 
 if __name__ == '__main__':
     from pprint import pprint

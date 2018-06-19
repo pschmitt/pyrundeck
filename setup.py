466a9262c2864d41a72be37b2f2c5239efc07020
Philipp Schmitt <philipp@schmitt.co>
2018-06-19 15:40:18 +0200

    Version 0.9.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyrundeck',
-    version='0.9.5',
+    version='0.9.6',
     description='Python REST API client for Rundeck 2.6+',
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',

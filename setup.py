c284bcf9368641cd85f4a2c5e814481cebc0d1e5
Philipp Schmitt <philipp@schmitt.co>
2020-05-21 10:36:47 +0200

    Set license

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name="pyrundeck",
     version="0.9.9",
+    license="GPL3",
     description="Python REST API client for Rundeck 2.6+",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",

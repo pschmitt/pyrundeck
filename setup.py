c8bc784f4c023b5eb6010d01d303a167d1428561
Philipp Schmitt <philipp@schmitt.co>
2021-11-10 15:28:27 +0100

    Version 0.9.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyrundeck",
-    version="0.9.12",
+    version="0.9.13",
     license="GPL3",
     description="Python REST API client for Rundeck 2.6+",
     long_description=open("README.md", "r").read(),
